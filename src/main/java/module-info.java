21828bc9b55aafe7d667c8c49b6b9addd8a5798e
Thomas Couchoud <zerderr@gmail.com>
2019-01-29 14:04:24 +0100

    Add base

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 2018-12-13.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-12-13
+ */
+module IA {
+	requires java.scripting;
+	requires org.slf4j;
+	requires rt;
+}
