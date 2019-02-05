2a1e67644d1920677919b9ae8e7c8223a52900e8
Thomas Couchoud <zerderr@gmail.com>
2019-02-05 10:07:43 +0100

    Use global S & N

--- src/main/java/fr/mrcraftcod/ia/Main.java
+++ src/main/java/fr/mrcraftcod/ia/Main.java
@@ -2,152 +2,151 @@ package fr.mrcraftcod.ia;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
  * @author hubert.cardot
  */
-public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
-    private final static int nbClasses = 3;
-    private final static int nbCaract = 4;
-    private final static int nbEx = 50;
-    private final static int nbExApprent = 25;
-    private final static int nbCouches = 3;
-    private final static int nbCaches = 6;
-    private final static int nbApprent = 2000;
-    private final static int[] nbNeurones = {
-            nbCaract + 1,
-            nbCaches + 1,
-            nbClasses
-    }; //+1 pour neurone fixe
-    private final static double[][][] data = new double[nbClasses][nbEx][nbCaract];
-    private final static double[][][] poids = new double[nbCouches - 1][][];
-    private final static double[][] N = new double[nbCouches][];
-    private final static double[][] S = new double[nbCouches][];
-    private final static double coeffApprent = 0.01;
-    private final static double coeffSigmoide = 2.0 / 3;
-
-    private static Double fSigmoide(Double x) {
-        return Math.tanh(coeffSigmoide * x);
-    }
-
-    private static Double dfSigmoide(Double x) {
-        return coeffSigmoide / Math.pow(Math.cosh(coeffSigmoide * x), 2);
-    }
-
-    public static void main(String[] args) {
-        LOGGER.info("Caches={} App={} coef={}", nbCaches, nbApprent, coeffApprent);
-        initialisation();
-        apprentissage();
-        evaluation();
-    }
-
-    private static void initialisation() {
-        readFile(Path.of("iris.data"));
-
-        for (int couche = 0; couche < nbCouches - 1; couche++) {
-            poids[couche] = new double[nbNeurones[couche + 1]][];
-            for (int i = 0; i < nbNeurones[couche + 1]; i++) {
-                poids[couche][i] = new double[nbNeurones[couche]];
-                for (int j = 0; j < nbNeurones[couche]; j++) {
-                    poids[couche][i][j] = (Math.random() - 0.5) / 10; //dans [-0,05; +0,05[
-                }
-            }
-        }
-
-        for (int couche = 0; couche < nbCouches; couche++) {
-            N[couche] = new double[nbNeurones[couche]];
-            S[couche] = new double[nbNeurones[couche]];
-        }
-    }
-
-    private static void apprentissage() {
-        for (int klass = 0; klass < nbClasses; klass++) {
-            for (int apprentIndex = 0; apprentIndex < nbExApprent; apprentIndex++) {
-                double[] results = applyNetwork(data[klass][apprentIndex]);
-                double[] errors = new double[results.length];
-                for (int errorIndex = 0; errorIndex < errors.length; errorIndex++) {
-                    errors[errorIndex] = Math.pow(results[errorIndex] - (klass == errorIndex ? 1 : 0), 2);
-                }
-                for (int errorIndex = 0; errorIndex < errors.length; errorIndex++) {
-                    retropropagation(errorIndex, errors[errorIndex]);
-                }
-            }
-        }
-    }
-
-    private static void retropropagation(int classe, double error) {
-        // TODO
-    }
-
-    private static double[] applyNetwork(double[] data) {
-
-        double[] resCouche = new double[nbNeurones[1]];
-        for (int j = 0; j < nbNeurones[1]; j++) {
-            for (int k = 0; k < nbNeurones[0]; k++) {
-                resCouche[j] += poids[0][j][k] * (k == 0 ? 1 : data[k - 1]);
-            }
-        }
-
-        data = resCouche;
-
-        for (int coucheIndex = 1; coucheIndex < nbCouches; coucheIndex++) {
-            resCouche = new double[nbNeurones[coucheIndex]];
-            for (int currentNeuroneIndex = 0; currentNeuroneIndex < nbNeurones[coucheIndex]; currentNeuroneIndex++) {
-                for (int neuronePrecedentIndex = 0; neuronePrecedentIndex < nbNeurones[coucheIndex - 1]; neuronePrecedentIndex++) {
-                    resCouche[currentNeuroneIndex] += poids[coucheIndex - 1][currentNeuroneIndex][neuronePrecedentIndex] * data[neuronePrecedentIndex];
-                }
-            }
-            data = resCouche;
-        }
-
-        return data;
-    }
-
-    private static void evaluation() {
-        int correctClass = 0;
-        int total = 0;
-        for (int klass = 0; klass < nbClasses; klass++) {
-            for (int exIndex = nbExApprent; exIndex < nbEx; exIndex++) {
-                double max = Double.MIN_VALUE;
-                int classe = -1;
-                double[] output = applyNetwork(data[klass][exIndex]);
-                for (int layerIndex = 0; layerIndex < output.length; layerIndex++) {
-                    if (output[layerIndex] > max) {
-                        max = output[layerIndex];
-                        classe = layerIndex;
-                    }
-                }
-                LOGGER.debug("Classe {} - classe trouvée {}", klass, classe);
-                if (klass == classe) {
-                    correctClass++;
-                }
-                total++;
-            }
-        }
-        LOGGER.info("Taux de reconnaissance : {}%", correctClass * 100.0 / total);
-    }
-
-    private static void readFile(Path path) {
-        int classe = 0;
-        int n = 0;
-        try {
-            for (String line : Files.readAllLines(path)) {
-                for (int caracIndex = 0; caracIndex < nbCaract; caracIndex++) {
-                    String valCarac = line.substring(caracIndex * nbCaract, caracIndex * nbCaract + 3);
-                    data[classe][n][caracIndex] = Double.parseDouble(valCarac);
-                }
-                if (++n == nbEx) {
-                    n = 0;
-                    classe++;
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.error("Error parsing file", e);
-        }
-    }
+public class Main{
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	
+	private final static int nbClasses = 3;
+	private final static int nbCaract = 4;
+	private final static int nbEx = 50;
+	private final static int nbExApprent = 25;
+	private final static int nbCouches = 3;
+	private final static int nbCaches = 6;
+	private final static int nbApprent = 2000;
+	private final static int[] nbNeurones = {
+			nbCaract + 1,
+			nbCaches + 1,
+			nbClasses
+	}; //+1 pour neurone fixe
+	private final static double[][][] data = new double[nbClasses][nbEx][nbCaract];
+	private final static double[][][] poids = new double[nbCouches - 1][][];
+	private final static double[][] N = new double[nbCouches][];
+	private final static double[][] S = new double[nbCouches][];
+	private final static double coeffApprent = 0.01;
+	private final static double coeffSigmoide = 2.0 / 3;
+	
+	private static Double fSigmoide(Double x){
+		return Math.tanh(coeffSigmoide * x);
+	}
+	
+	private static Double dfSigmoide(Double x){
+		return coeffSigmoide / Math.pow(Math.cosh(coeffSigmoide * x), 2);
+	}
+	
+	public static void main(String[] args){
+		LOGGER.info("Caches={} App={} coef={}", nbCaches, nbApprent, coeffApprent);
+		initialisation();
+		apprentissage();
+		evaluation();
+	}
+	
+	private static void initialisation(){
+		readFile(Path.of("iris.data"));
+		
+		for(int couche = 0; couche < nbCouches - 1; couche++){
+			poids[couche] = new double[nbNeurones[couche + 1]][];
+			for(int i = 0; i < nbNeurones[couche + 1]; i++){
+				poids[couche][i] = new double[nbNeurones[couche]];
+				for(int j = 0; j < nbNeurones[couche]; j++){
+					poids[couche][i][j] = (Math.random() - 0.5) / 10; //dans [-0,05; +0,05[
+				}
+			}
+		}
+		
+		for(int couche = 0; couche < nbCouches; couche++){
+			N[couche] = new double[nbNeurones[couche]];
+			S[couche] = new double[nbNeurones[couche]];
+		}
+	}
+	
+	private static void apprentissage(){
+		for(int klass = 0; klass < nbClasses; klass++){
+			for(int apprentIndex = 0; apprentIndex < nbExApprent; apprentIndex++){
+				double[] results = applyNetwork(data[klass][apprentIndex]);
+				double[] errors = new double[results.length];
+				for(int errorIndex = 0; errorIndex < errors.length; errorIndex++){
+					errors[errorIndex] = Math.pow(results[errorIndex] - (klass == errorIndex ? 1 : 0), 2);
+				}
+				for(int errorIndex = 0; errorIndex < errors.length; errorIndex++){
+					retropropagation(errorIndex, errors[errorIndex]);
+				}
+			}
+		}
+	}
+	
+	private static void retropropagation(int classe, double error){
+		// TODO
+	}
+	
+	private static double[] applyNetwork(double[] data){
+		/* Init first nodes */
+		N[0][0] = 1;
+		System.arraycopy(data, 0, N[0], 1, data.length);
+		for(int i = 0; i < N[0].length; i++){
+			S[0][i] = fSigmoide(N[0][i]);
+		}
+		
+		/* Propagate N & S values to other layers */
+		for(int layerTo = 1; layerTo < nbCouches; layerTo++){
+			for(int neuroneTo = 0; neuroneTo < nbNeurones[layerTo]; neuroneTo++){
+				var n = 0D;
+				for(int neuroneFrom = 0; neuroneFrom < nbNeurones[layerTo - 1]; neuroneFrom++){
+					n += poids[layerTo - 1][neuroneTo][neuroneFrom] * S[layerTo - 1][neuroneFrom];
+				}
+				N[layerTo][neuroneTo] = n;
+				S[layerTo][neuroneTo] = fSigmoide(n);
+			}
+		}
+		
+		return data;
+	}
+	
+	private static void evaluation(){
+		int correctClass = 0;
+		int total = 0;
+		for(int klass = 0; klass < nbClasses; klass++){
+			for(int exIndex = nbExApprent; exIndex < nbEx; exIndex++){
+				double max = Double.MIN_VALUE;
+				int classe = -1;
+				double[] output = applyNetwork(data[klass][exIndex]);
+				for(int layerIndex = 0; layerIndex < output.length; layerIndex++){
+					if(output[layerIndex] > max){
+						max = output[layerIndex];
+						classe = layerIndex;
+					}
+				}
+				LOGGER.debug("Classe {} - classe trouvée {}", klass, classe);
+				if(klass == classe){
+					correctClass++;
+				}
+				total++;
+			}
+		}
+		LOGGER.info("Taux de reconnaissance : {}%", correctClass * 100.0 / total);
+	}
+	
+	private static void readFile(Path path){
+		int classe = 0;
+		int n = 0;
+		try{
+			for(String line : Files.readAllLines(path)){
+				for(int caracIndex = 0; caracIndex < nbCaract; caracIndex++){
+					String valCarac = line.substring(caracIndex * nbCaract, caracIndex * nbCaract + 3);
+					data[classe][n][caracIndex] = Double.parseDouble(valCarac);
+				}
+				if(++n == nbEx){
+					n = 0;
+					classe++;
+				}
+			}
+		}
+		catch(Exception e){
+			LOGGER.error("Error parsing file", e);
+		}
+	}
 }
